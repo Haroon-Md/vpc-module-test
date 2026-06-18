82e4dec6745ec323e4789e9783ca425c3a7d7c98
haroon <haroonbashamohammad@gmail.com>
2026-06-18 23:19:39 +0530

    created vpc

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+    source = "../terraform-aws-vpc"
+    project_name = var.project_name
+    environment = var.environment
+    vpc_cidr = var.vpc_cidr
+    vpc_tags = var.vpc_tags
+
+
+    #public subnets
+    public_subnet_cidrs = var.public_subnet_cidr
+    #private subnets
+    private_subnet_cidrs = var.private_subnet_cidr
+
+    #database subnets
+    database_subnet_cidrs = var.database_subnet_cidr
+}
---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "haroon-practice-terraform-1"
+    key    = "vpc-module-demo"
+    use_lockfile = true
+    encrypt = true
+    region = "us-east-1"
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
